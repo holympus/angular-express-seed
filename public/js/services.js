240db5b4d6aa7c4c84a57ce92666e3c6fbf426c4
Alex Lockwood <alex.lockwood@gmail.com>
2013-09-28 15:20:26 -0700

    cleanup and commenting of services

--- public/js/services.js
+++ public/js/services.js
@@ -7,38 +7,55 @@
 // In this case it is a simple value service.
 angular.module('myApp.services', ['ngResource','ng'])
 
+
+//CONSTANTS
 .value('version', '0.1')
 
+
+
+// RESOURCE FOR LOGGED IN USER OBJECT
+// get  => returns userObject
+// save => post method, logins in user with {username,password}
+// delete => logout user
 .factory('LoggedInResource', ['$resource' , function($resource){
   this.Login = $resource ("/api/login");
   this.Login.username = '';
   return this.Login; 
 }])
 
+
+// SERVICE TO HANDLE LOGGING IN AND OUT
+
 .factory("LoggedIn", ['LoggedInResource', '$location', function(resource,$location){
   var self = this; 
-  this.first_name = '';
-  this.username = '';
-  this.success = '';
-  this.message = '';
-  this.status = ''; 
+  self.first_name = self.username = self.success = self.message = self.status = null; 
+
+  console.log(this); 
+  //Common Private functions
+  var setUserData = function(data){
+    console.log(data); 
+    if(data !== {} && !!data){
+      self.first_name = data.first_name; 
+      self.username = data.username;
+      self.success = data.success;
+      self.message = data.message || "Successfully Logged In as " + data.username;
+      self.status = data.status; 
+    }else{
+      //nullify data
+      self.first_name = self.username = self.success = self.message = self.status = null; 
+    }
+  } 
   
+  //init the userdata object
+  setUserData(null);
   this.doLogin = function(data){
     var username = data.username;
     resource.save(data, function(data,headers){
-      console.log('loggedIn resp',data);
-      console.log('headers',headers);
-      self.first_name = data.first_name; 
-      self.username = username;
-      self.success = data.success;
-      self.message = data.message;
-
+      data.username = data.username || username; 
+      setUserData(data);
     },function(err){
       console.log('loginerr',err);
-      self.message = err.data; 
-      self.status = err.status; 
-      self.failed = true; 
-      console.log('');
+      setUserData(err);
     });
   };
   
@@ -49,11 +66,7 @@ angular.module('myApp.services', ['ngResource','ng'])
       console.log('loggout backend error', err);
     });
     console.log('starting to log out');
-    self.first_name = '';
-    self.username = '';
-    self.success = '';
-    self.message = '';
-    self.status = ''; 
+    setUserData(null);
     $location.path('/login');
   }
   
@@ -72,19 +85,15 @@ angular.module('myApp.services', ['ngResource','ng'])
     console.log('initializing user', self);
     resource.get({},function(data,headers){
       console.log('verified user initiated');
-      self.first_name = data.first_name; 
-      self.username = data.username;
-      self.success = data.success;
-      self.message = data.message || "Successfully Logged In as " + data.username;
-      
-    },function(err){
+      setUserData(data);    
+    },function(err){ //not logged in
+      setUserData(null);
       $location.path('/login')
     });
   }
   
+ 
   return this; 
 }])
-// get  => returns userObject
-// save => post method, logins in user with {username,password}
-// delete => logout user
+
 
