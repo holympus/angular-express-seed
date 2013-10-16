a683797862050af74beef1e7320931c44c044805
Alex Lockwood <alex.lockwood@gmail.com>
2013-10-16 10:21:25 -0700

    question directive

--- public/js/directives.js
+++ public/js/directives.js
@@ -3,8 +3,39 @@
 /* Directives */
 
 angular.module('myApp.directives', []).
-  directive('appVersion', function (version) {
+  directive('appVersion', ['version',function (version) {
     return function(scope, elm, attrs) {
       elm.text(version);
     };
-  });
+  }]).
+  
+  directive('question',[function(){
+    return {
+
+      restrict: 'A',
+      priorety: 0,
+      templateUrl: 'partials/common/question', //expects this to be loaded on the page
+      // compile: function(tEl, tAttrs, transclude){
+      //     console.log('element',tElement);
+      //     console.log('tAttrs',tAttrs);
+      //     console.log('transclude', transclude);
+      //     return{
+      //       pre: function preLink(scope, el, attrs) {
+      //         console.log('attrs', iAttrs);
+      //       },
+      //       post: function postLink(scope,el,attrs){
+      //
+      //       }
+      //     };
+      // },
+      link: function(scope, element, attrs){
+        console.log('scope',scope);
+      },
+      
+
+    
+    };
+    
+  }])
+  
+  
